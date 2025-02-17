0650f794917ecfd6d278bb4a959e6c6845586190
lucastorres520 <120499082+lucastorres520@users.noreply.github.com>
2025-02-16 22:22:47 -0500

    Update models.py

--- models.py
+++ models.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import relationship, declarative_base
 Base = declarative_base()
 
 class User(Base):
+    """
+    User model representing users in the database.
+    """
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
@@ -11,11 +14,16 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
 
+    items = relationship("Item", back_populates="owner")
+
 class Item(Base):
+    """
+    Item model representing items owned by users.
+    """
     __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
-    owner = relationship("User")
+    owner = relationship("User", back_populates="items")
