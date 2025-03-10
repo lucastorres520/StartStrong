a9d70aaae024aa49fc252f156b037501faa6fea4
lucastorres520 <120499082+lucastorres520@users.noreply.github.com>
2025-03-09 21:34:53 -0400

    Delete database.py

--- database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-# SQLite database connection
-DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
